3372efe477766543f0866f418f788613a85aafce
Francarlos Fernandez <FFernandez@ZorusTech.com>
2025-07-30 14:01:34 -0400

    feat(dnsf-extensions): Added a new DNSF Extension function to expose internal functionality to the Windows Agent.

---
+++ ARSoft.Tools.Net/Dns/Dnsf/DnsfDnsMessageBaseExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace ARSoft.Tools.Net.Dns;
+
+public static class DnsfDnsMessageBaseExtensions
+{
+    public static ArraySegment<byte> Encode(this DnsMessageBase message, bool includeLengthPrefix)
+        => message.Encode().ToArraySegment(includeLengthPrefix);
+}
