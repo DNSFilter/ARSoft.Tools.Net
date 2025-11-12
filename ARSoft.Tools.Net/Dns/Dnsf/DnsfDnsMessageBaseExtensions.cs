d26f52d07c2cc7411354fd95afa543b9d6a79c4c
Radu Andrei Solomon <rsolomon@dnsfilter.com>
2025-11-12 10:51:58 -0500

    fix: minor fixes

---
+++ ARSoft.Tools.Net/Dns/Dnsf/DnsfDnsMessageBaseExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace ARSoft.Tools.Net.Dns;
+
+public static class DnsfDnsMessageBaseExtensions
+{
+    public static int Encode(this DnsMessageBase message, bool includeLengthPrefix, out byte[] messageData)
+        => message.Encode(includeLengthPrefix, out messageData);
+}
